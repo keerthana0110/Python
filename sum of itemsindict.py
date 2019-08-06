a38d80c5ef1a1f31740d390b2cefc49416dd143f
Keerthana.T <48148816+keerthana0110@users.noreply.github.com>
2019-08-06 01:54:09 -0700

    Create sum of itemsindict.py

---
+++ sum of itemsindict.py
@@ -0,0 +1,11 @@
+def returnSum(dict): 
+      
+     sum = 0
+     for i in dict.values(): 
+           sum = sum + i 
+       
+     return sum
+  
+
+dict = {'a': 100, 'b':200, 'c':300} 
+print("Sum :", returnSum(dict)) 
